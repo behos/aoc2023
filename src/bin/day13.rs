bc4b64ac15c8f9ee6c69f1f13493d2b689a62cc3
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2024-10-13 18:00:02 +0300

    Day 13 pt 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -5,13 +5,18 @@ use anyhow::{Context, Error, Result};
 fn main() -> Result<()> {
     let raw = read_to_string("inputs/13.txt").context("Should have been able to read the file")?;
     let raw = raw.trim();
-    let patterns = raw
+    let mut patterns = raw
         .split("\n\n")
         .map(|r| Pattern::from_str(r).context("unparseable pattern"))
         .collect::<Result<Vec<Pattern>>>()?;
     let pt1 = patterns.iter().map(Pattern::mirror).sum::<usize>();
     println!("part 1: {pt1}");
-    // println!("part 2: {total}");
+
+    let pt2 = patterns
+        .iter_mut()
+        .map(Pattern::smudged_mirror)
+        .sum::<usize>();
+    println!("part 2: {pt2}");
     Ok(())
 }
 
@@ -40,20 +45,47 @@ impl FromStr for Pattern {
 
 impl Pattern {
     fn mirror(&self) -> usize {
-        let row_mirror = find_mirror(&self.by_row);
-        let column_mirror = find_mirror(&self.by_column);
+        let row_mirror = find_mirror(&self.by_row, None);
+        let column_mirror = find_mirror(&self.by_column, None);
         row_mirror + 100 * column_mirror
+    }
+
+    fn smudged_mirror(&mut self) -> usize {
+        let mirror = self.mirror();
+        let exclude_x = Some(mirror % 100);
+        let exclude_y = Some(mirror / 100);
+        for x in 0..self.by_row.len() {
+            for y in 0..self.by_column.len() {
+                let old = self.by_row[x][y];
+                let new = if old == '.' { '#' } else { '.' };
+                self.by_row[x][y] = new;
+                let row_mirror = find_mirror(&self.by_row, exclude_x);
+                self.by_row[x][y] = old;
+                if row_mirror > 0 {
+                    return row_mirror;
+                }
+                self.by_column[y][x] = new;
+                let column_mirror = find_mirror(&self.by_column, exclude_y);
+                self.by_column[y][x] = old;
+                if column_mirror > 0 {
+                    return column_mirror * 100;
+                }
+            }
+        }
+        panic!("no solution")
     }
 }
 
-fn find_mirror(pattern: &[Vec<char>]) -> usize {
-    pattern
+fn find_mirror(pattern: &[Vec<char>], exclude: Option<usize>) -> usize {
+    let mut candidates = pattern
         .iter()
         .map(|p| mirror_points(p))
         .reduce(|acc, e| acc.intersection(&e).cloned().collect())
-        .expect("must have a set")
-        .pop_first()
-        .unwrap_or(0)
+        .expect("must have a set");
+    if let Some(exclude) = exclude {
+        candidates.remove(&exclude);
+    }
+    candidates.pop_first().unwrap_or(0)
 }
 
 fn mirror_points(line: &[char]) -> BTreeSet<usize> {
